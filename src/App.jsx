288ad6ac1af7d7a9e28790033c2629276d84d8d0
iArijitGanguly <arijeetGanguly63@gmail.com>
2024-07-17 23:54:18 +0530

    Slight change in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-// import CommentContainer from "./components/CommentContainer"
 import CommentReplyContainer from "./components/CommentReplyContainer";
 
 function App() {
   
   return (
     <>
-      {/* <CommentContainer /> */}
-      {/* <CommentMsg isReplying={isReplying} setIsReplying={setIsReplying} /> */}
       <CommentReplyContainer />
     </>
   )
